749c69298c17684f4c764a5d06607cb05d7ad7b1
kurimetsu <wesley0706wr@gmail.com>
2026-08-21 12:18:10 -0300

    mudança pro render

--- backend/server.js
+++ backend/server.js
@@ -154,6 +154,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-app.listen(3000, () => {
-  console.log("🏥 Hospital Aura rodando em http://localhost:3000");
+const PORT = process.env.PORT
+             || 3000;
+app.listen(PORT, () => {
+  console.log(`Porta ${PORT}`);
 });
